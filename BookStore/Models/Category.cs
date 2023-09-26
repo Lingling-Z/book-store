901932e529301b7eef6ccf4452a46e40642363d2
Lingling-Z <97594629+Lingling-Z@users.noreply.github.com>
2023-09-26 13:54:02 +1300

    category crud

--- BookStore/Models/Category.cs
+++ BookStore/Models/Category.cs
@@ -6,7 +6,11 @@ namespace BookStore.Models
     {
         public int Id { get; set; }
         [Required]
+        [MaxLength(30)]
+        [Display(Name = "Category Name")]
         public string Name { get; set; }
+        [Range(1, 100)]
+        [Display(Name = "Display Order")]
         public int DisplayOrder { get; set; }
     }
 }
